b40d7e849f006f0e457eb67d306951b4634e89b7
Kael <76127710@qq.com>
2014-08-11 21:49:36 +0800

    Initial

---
+++ app/views/projects/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @project, :id, :title, :description, :image_url, :point, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Leed::Application.config.secret_key_base = '45e5464f374738b4210147845aa9f0286e060a7bba99a63f1b89b226f88f486db8cd5217e31ab70230d602a9eb90bdb5fff67eeb69af6dfb3f476484d1f8cbb5'
